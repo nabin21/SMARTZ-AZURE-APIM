7a115b019798903f3282cf86ae3a99104e750660
wxp <wangxiaoping@shinetechsoftware.com>
2024-02-21 19:20:53 +0800

    update management api endpoint

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ const {deployNodeJS} = require("@azure/api-management-custom-widgets-tools")
 
 const serviceInformation = {
 	"resourceId": "subscriptions/b6c083b7-9073-461d-8fc6-8b8bf82e100b/resourceGroups/SmartzAPPWestEurope/providers/Microsoft.ApiManagement/service/smartz-api",
-	"managementApiEndpoint": "https://management.usgovcloudapi.net"
+	"managementApiEndpoint": "https://management.azure.com"
 }
 const name = "cw-register-application-form"
 const fallbackConfigPath = "./static/config.msapim.json"
